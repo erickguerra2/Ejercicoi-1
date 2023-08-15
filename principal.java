d5027934d577dc5c823afd85f439ee373b676f88
erickguerra2 <92761694+erickguerra2@users.noreply.github.com>
2023-08-15 00:11:44 -0600

    Correciones

--- principal.java
+++ principal.java
@@ -53,29 +53,29 @@ public class principal {
                         System.out.println("El solicitud " + solicitud + " es apto para compra.");
                         Random randomLocalidad = new Random();
                         int seleccionLocalidad = randomLocalidad.nextInt(3) + 1; // 1, 2 o 3
-                        Localidad localidadSeleccionada = null;
+                        Localidad localidadr = null;
                         switch (seleccionLocalidad) {
                             case 1:
-                                localidadSeleccionada = localidad1;
+                                localidadr = localidad1;
                                 break;
                             case 2:
-                                localidadSeleccionada = localidad5;
+                                localidadr = localidad5;
                                 break;
                             case 3:
-                                localidadSeleccionada = localidad10;
+                                localidadr = localidad10;
                                 break;
                             default:
                                 System.out.println("Error en la selección de localidad.");
                         }
-                        if (localidadSeleccionada != null) {
-                            int boletosDisponibles = localidadSeleccionada.getBoletosDisponibles();
+                        if (localidadr != null) {
+                            int boletosDisponibles = localidadr.getBoletosDisponibles();
                             int boletosAVender = Math.min(cantidadBoletos, boletosDisponibles);
-                            int precioTotal = boletosAVender * localidadSeleccionada.getPrecio();
+                            int precioTotal = boletosAVender * localidadr.getPrecio();
                             if (precioTotal > comprador.getpresupuesto()) {
                                 System.out.println("El precio total excede el presupuesto del comprador. Compra cancelada.");
                             } else {
-                                localidadSeleccionada.venderBoletos(boletosAVender);
-                                System.out.println("Compra exitosa: " + boletosAVender + " boletos en " + localidadSeleccionada.getNombre() + " por $" + precioTotal);
+                                localidadr.venderBoletos(boletosAVender);
+                                System.out.println("Compra exitosa: " + boletosAVender + " boletos en " + localidadr.getNombre() + " por $" + precioTotal);
                             }
                         }
                     } else {
@@ -89,31 +89,31 @@ public class principal {
                     System.out.println(localidad10.getNombre() + ": Vendidos " + localidad10.getBoletosVendidos() + ", Disponibles " + localidad10.getBoletosDisponibles());
                     break;
                 case 4:
-                    System.out.println("Seleccione una localidad para consultar disponibilidad (1: Localidad 1, 2: Localidad 5, 3: Localidad 10): ");
+                    System.out.println("Seleccione una localidad (1: Localidad 1, 2: Localidad 5, 3: Localidad 10): ");
                     int seleccionLocalidad = scanner.nextInt();
-                    Localidad localidadSeleccionada = null;
+                    Localidad localidadr = null;
                     switch (seleccionLocalidad) {
                         case 1:
-                            localidadSeleccionada = localidad1;
+                            localidadr = localidad1;
                             break;
                         case 2:
-                            localidadSeleccionada = localidad5;
+                            localidadr = localidad5;
                             break;
                         case 3:
-                            localidadSeleccionada = localidad10;
+                            localidadr = localidad10;
                             break;
                         default:
                             System.out.println("Selección inválida.");
                     }
-                    if (localidadSeleccionada != null) {
-                        System.out.println("Disponibilidad en " + localidadSeleccionada.getNombre() + ": " + localidadSeleccionada.getBoletosDisponibles() + " boletos disponibles.");
+                    if (localidadr != null) {
+                        System.out.println("Disponibilidad en " + localidadr.getNombre() + ": " + localidadr.getBoletosDisponibles() + " boletos disponibles.");
                     }
                     break;
                 case 5:
-                    int ingresosTotales = (localidad1.getBoletosVendidos() * localidad1.getPrecio()) +
+                    int total_ganancias = (localidad1.getBoletosVendidos() * localidad1.getPrecio()) +
                                          (localidad5.getBoletosVendidos() * localidad5.getPrecio()) +
                                          (localidad10.getBoletosVendidos() * localidad10.getPrecio());
-                    System.out.println("Reporte de caja: Ingresos totales $" + ingresosTotales);
+                    System.out.println("Reporte de caja: Ingresos totales $" + total_ganancias);
                     break;
                 case 6:
                     System.out.println("¡Hasta luego!");
